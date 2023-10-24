5d6b4b277683ba58dbdde6cd9a47369e4e3afd42
carmensanchez19 <msanmar508@iesalmudeyne.es>
2023-10-24 14:23:24 +0200

    create setters y getters

--- pareja3.java
+++ pareja3.java
@@ -3,11 +3,41 @@ public class Proyecto{
     public String nombre;
     public String descripcion;
     public int codigo;
-    
+
     constructor(nombre, descripcion, codigo){
         super();
         this.nombre=nombre;
         this.decripcion=descripcion;
         this.codigo=codigo;
+    }
+
+    // Método getter para obtener el valor de 'nombre'
+    public String getNombre() {
+        return nombre;
+    }
+
+    // Método setter para establecer el valor de 'nombre'
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    // Método getter para obtener el valor de 'descripcion'
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    // Método setter para establecer el valor de 'descripcion'
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    // Método getter para obtener el valor de 'codigo'
+    public int getCodigo() {
+        return codigo;
+    }
+
+    // Método setter para establecer el valor de 'codigo'
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
     }
 }
